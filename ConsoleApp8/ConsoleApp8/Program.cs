4f1f1b19b33a52e6ba736b14be092ebdbb786118
Elev <Elev@TEB123013>
2019-10-21 12:25:27 +0200

    meotd

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        public static void main()
+        {
+            Console.WriteLine("skriv ett tal");
+            float tal1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("skriv ett till");
+            float tal2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("skriv ett till");
+            float tal3 = float.Parse(Console.ReadLine());
+            Console.WriteLine("medelvärdet är" + BeräknaMedel(tal1, tal2, tal3));
+        }
+
+
+
+        public float BeräknaMedel(float tal1, float tal2, float tal3)
+        {
+            float a = (tal1 + tal2 + tal3) / 3;
+            return a;
+        }
+    }
+}
